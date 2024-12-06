9bf0beddf82f6481b360dc1dc6b0760d178ee3e6
Munna SR <munnaofficialnumbtk@gmail.com>
2024-12-07 01:53:52 +0600

    prime

--- Final Lab test/arraysorted.java
+++ Final Lab test/arraysorted.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class pallindrome {
+public class arraysorted {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         System.out.println("Enter array size: ");
--- Final Lab test/factorial.java
+++ Final Lab test/factorial.java
@@ -2,21 +2,20 @@ import java.util.Scanner;
 
 public class factorial {
     public static void main(String[] args) {
-        
-    
-     Scanner input=new Scanner(System.in);
+
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter a number: ");
-        
-        int num=input.nextInt();
-        if(num<0){
+
+        int num = input.nextInt();
+        if (num < 0) {
             System.out.println("please give positive value");
-        }else{
-        int fact=1;
-        for(int i=num;i>0;i--){
-            fact*=i;
+        } else {
+            int fact = 1;
+            for (int i = num; i > 0; i--) {
+                fact *= i;
+            }
+            System.out.println("fcatorial: " + fact);
+            input.close();
         }
-        System.out.println("fcatorial: "+fact);
-        input.close();
-}
-}
+    }
 }
