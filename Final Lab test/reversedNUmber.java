4272b7c156aadbd070ba7bab8562f9ec3cc0bc88
Munna SR <munnaofficialnumbtk@gmail.com>
2024-12-06 12:38:29 +0600

    Reversed Array

--- Final Lab test/reversedNUmber.java
+++ Final Lab test/reversedNUmber.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
 
-
 public class reversedNUmber {
-
     public static void main(String[] args) {
-        Scanner input =new Scanner(System.in);
-        System.out.println("enter a number: ");
 
+        Scanner input = new Scanner(System.in);
 
-        int size=input.nextInt();
-        int arr[]=new int[size];
+        System.out.print("Enter arry size: ");
 
-        for(int i=0;i<size;i++){
-     System.out.println(i);
-
+        int size = input.nextInt();
+        int arr[] = new int[size];
+        System.out.println("enter the element: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = input.nextInt();
         }
+
+        System.out.println("reversed: ");
+
+        for (int i = size - 1; i >= 0; i--) {
+            System.out.println(arr[i] + " ");
+        }
+
+        input.close();
     }
 }
