473cdbfa135430008487f400bedb7b039079268d
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-01-31 19:04:10 +0600

    Method Overloading

---
+++ Closer Look on Class/MethodOvL.java
@@ -0,0 +1,26 @@
+class OverloadDemo {
+    void test(){
+        System.out.println("NO Parameters");
+    }
+    void test(int a){
+        System.out.println("a: "+a);
+    }
+    void test(int a,int b){
+        System.out.println("a value: "+a+" b value : "+b);
+    }
+   double  test(double a){
+    System.out.println("double: "+a);
+        return a*a;
+    }
+}
+class MethodOvL {
+    public static void main (String args[]){
+        OverloadDemo ob=new OverloadDemo();
+        double result;
+        ob.test();
+        ob.test(10,20);
+        ob.test(10);
+        result=ob.test(123.5);
+
+    }
+}
