f8d0acb68c46cc5934765444dd3af752500557d1
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-02-06 18:37:19 +0600

    Basic Inheritance

---
+++ Lab Assignment 05/Problem01.java
@@ -0,0 +1,33 @@
+class Animal {
+    String name;
+    int age;
+    
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+class Dog extends Animal {
+    
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+    
+    public void bark() {
+        System.out.println(name + " is barking!");
+    }
+}
+
+public class Problem01 {
+    public static void main(String[] args) {
+        Dog myDog = new Dog("Buddy", 3);
+        myDog.displayInfo();
+        myDog.bark();
+    }
+}
