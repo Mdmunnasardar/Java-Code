8a62b186ba3271c905ad9ebe92383a254d1efa06
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-02-22 01:36:25 +0600

    ob paassing

---
+++ lab final 2/passingob.java
@@ -0,0 +1,22 @@
+class Car { 
+    String model; 
+ 
+    public Car(String model) { 
+        this.model = model; 
+    } 
+ 
+    public void CarName(Car car) { 
+        System.out.println("This car is a " + car.model); 
+    } 
+} 
+public class passingob { 
+    public static void main(String args[]) { 
+         
+        Car car1 = new Car("Toyota"); 
+        Car car2 = new Car("Honda"); 
+ 
+        car1.CarName(car2);  
+        car2.CarName(car1);  
+    } 
+} 
+ 
