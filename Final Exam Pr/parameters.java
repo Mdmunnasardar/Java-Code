b49e85b5cd60a15c9a1314230ac31be732f5e87f
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-03-04 01:38:02 +0600

    parameter method

---
+++ Final Exam Pr/parameters.java
@@ -0,0 +1,37 @@
+public class parameters {
+    double width;
+    double height;
+    double depth;
+
+    // void volume() { // Fixed method name
+    // System.out.println(width * height * depth);
+    // } this void which nothintg return now using return method value
+
+    double volume() {
+        return width * height * depth;
+    }
+    // para meter method which is passing argument in ()--
+
+    void setpara(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    public static void main(String[] args) {
+        parameters ob = new parameters(); // Fixed object instantiation
+        // now creating multiple obj to use it --
+        parameters ob1 = new parameters();
+        // double vol ;
+
+        ob.setpara(3, 3, 3);
+        ob.volume();
+        System.out.println(ob.volume());
+
+        ob1.setpara(30, 30, 30);
+        ob1.volume();
+        System.out.println(ob1.volume());
+
+        // Fixed method call
+    }
+}
