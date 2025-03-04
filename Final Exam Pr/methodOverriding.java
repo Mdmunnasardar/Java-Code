1649d2b0af141c244d45a6d14d9e9225f4b0a554
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-03-04 21:18:03 +0600

    just modifyinf something

--- Final Exam Pr/methodOverriding.java
+++ Final Exam Pr/methodOverriding.java
@@ -7,7 +7,7 @@ class A{
     }
 
     void show (){
-        System.out.println(" a and b are : "+a+" "+b);
+        System.out.println("Super class value called of a and b are : "+a+" "+b);
     }
 }
 
@@ -21,14 +21,20 @@ class B extends A{
 
     //@methodOverriding// as same name method in super class 
     void show(){
+        super.show();
         System.out.println("k : "+k);
     }
-}
+    void volume(){
+        System.out.println("Sum of three integer: " +a+b+k);
+    }
+    }
+
 
 public class methodOverriding {
     public static void main(String[] args) {
         B o= new B(1,2,3);
 
         o.show();
+        o.volume();
     }
 }
