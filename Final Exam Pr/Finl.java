542ffccee48061296cdea350a8cd8233a5cca333
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-03-04 14:33:25 +0600

    Final keyword that fixed

---
+++ Final Exam Pr/Finl.java
@@ -0,0 +1,10 @@
+public class Finl {
+    final int a=5;
+
+    public static void main(String[] args) {
+        Finl ob = new Finl();
+
+        //ob.a=20;//compile time error 
+        System.out.println(ob.a);
+    }
+}
